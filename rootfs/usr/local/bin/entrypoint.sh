934b04fa468b7695fabe667d57cb0697846d0822
11notes <github@11notes.ch>
2023-02-13 23:51:52 +0100

    add REACT_PORT

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/ash
 cd /app
 
+if [[ -z "${REACT_PORT}" ]]; then
+  REACT_PORT=3000
+fi
+
 if [ -z "${1}" ]; then
+    sed -ir "s/\"start\": \"react-scripts start\"/\"start\": \"PORT=${REACT_PORT} react-scripts start\"/" /app/package.json
     set -- "npm" start
 else
     case "${1}" in
